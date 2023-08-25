dd7ed2942bd7618bca742cda4b7418de37f86a76
Krzysztof Ligeska <krzysztof.ligeska@tietoevry.com>
2023-08-25 12:28:52 +0200

    Initial changes

---
+++ ProducerConsumerRabbitMQ/ProducerRabbitMQ/Sender.cs
@@ -0,0 +1,23 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace ProducerRabbitMQ
+{
+    public class Sender
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            var connection = factory.CreateConnection();
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("BasicTest",false,false,false,null);
+                string message = "Hello RabbitMQ to ja Ksysio";
+                var  body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish("", "BasicTest", false,null, body);
+                Console.WriteLine($"sent message: {message}");
+
+            }
+        }
+    }
+}
