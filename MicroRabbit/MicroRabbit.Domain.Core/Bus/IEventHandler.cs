7e8bf592e3122dccf3004c4d21fcd9f7765d1bc3
Krzysztof Ligeska <krzysztof.ligeska@tietoevry.com>
2023-09-11 17:31:01 +0200

    Domain core interfaces and classes

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using MicroRabbit.Domain.Core.Events;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    public interface IEventHandler<in TEvent> : IEventHadler where TEvent : Event
+    {
+        Task Handle(TEvent @event);
+    }
+
+    public interface IEventHadler
+    {
+    }
+}
