4280d3ba94bba5af7d8d552bf7ddffadc07cf51c
Krzysztof Ligeska <krzysztof.ligeska@tietoevry.com>
2023-09-21 12:56:59 +0200

    Initial commit of banking Microservice

---
+++ Rabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,22 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus,RabbitMQBus>();
+
+
+        }
+    }
+}
